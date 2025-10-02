0e9f5614241bb0c9d0aed5c837e2cd5c8ada6e59
Abdulmuiz Ghori <mrabdulmuizghori@gmail.com>
2025-10-03 00:45:50 +0530

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#04211A',
+        secondary: '#D8AD61',
+        secondary_light: '#FAF7F0',
+        secondary_light_2: '#FFF9E7',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
