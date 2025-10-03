3124297fdda31300d06b9bcf04cc48ac3b520aed
Abdulmuiz Ghori <mrabdulmuizghori@gmail.com>
2025-10-03 21:36:09 +0530

    All recitications

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         secondary: '#D8AD61',
         secondary_light: '#FAF7F0',
         secondary_light_2: '#FFF9E7',
+        tertiary: '#615E52',
       },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
